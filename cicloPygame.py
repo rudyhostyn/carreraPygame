8547df884b3a67c9790ef632d86e7d5508dafd4d
Administración <rudy@lacabinagris.com>
2021-04-17 07:28:53 +0200

    Carrera con todos los corredores

--- cicloPygame.py
+++ cicloPygame.py
@@ -1,52 +1,65 @@
 import pygame, sys
-import random
+import random2
 
 class Runner():
-    
+   
     def __init__(self, x=0, y=0):
-        self.custom = pygame.image.load("images/smallball.png")
+        self.custom =""
         self.position = [x,y]
-        self.name = "Madrid"
+        self.name = ""
     
     def avanzar(self):
-        self.position[0] += random.randint(1,6)
+        self.position[0] += random2.randint(1,6)
+        pygame.time.delay(10)
 
 class Game():
     runners = []
-    __startLine = -5
-    __finishLine = 600
+    __posY =(160, 200, 240, 280)
+    __names = ("¡Hala Madrid!", "Força Barça", "Che em guanyat", "Ahiva la ostia")
+    __customs = ('Madrid', 'Barcelona', 'Valencia', 'Bilbao')
+    __startLine = 20
+    __finishLine = 560
     
+        
     def __init__(self):
-        self.__screen = pygame.display.set_mode((640, 480))
+        self.__screen = pygame.display.set_mode((628, 402))
         #self.__screen.fill((226,147,48))
         self.__background = pygame.image.load("images/campoFutbol.jpg")
         pygame.display.set_caption("Carrera pygame")
         
-        firstRunner = Runner(self.__startLine,240)
-        firstRunner.name = "seepdy"
-        self.runners.append(firstRunner)
-        
-                
+        for i in range(4):
+            theRunner = Runner(self.__startLine, self.__posY[i])
+            theRunner.name = self.__names[i]
+            theRunner.custom = pygame.image.load("images/{}.png".format(self.__customs[i]))
+            self.runners.append(theRunner)    
+
     def competir(self):
         gameOver = False
         while not gameOver:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameOver = True
-    #refresco pantalla tras el evento
+            #refresco pantalla tras el evento
             self.__screen.blit(self.__background,(0,0))
-            self.__screen.blit(self.runners[0].custom, self.runners[0].position)
-            #empiezo a jugar con la funcion avanzar creada en la clase runner0
-            self.runners[0].avanzar()
             
-            if self.runners[0].position[0] >= self.__finishLine:
-                print("Ha ganado {}".format(self.runners[0].name))
-                gameOver = True
+            for i in self.runners:
+                self.__screen.blit(i.custom, i.position)
+            #empiezo a jugar con la funcion avanzar creada en la clase runner0
+            for activeRunner in self.runners:
+                activeRunner.avanzar()
+            # acaba el juego + mensaje del ganador   
+            for activeRunner in self.runners:
+                if activeRunner.position[0] >= self.__finishLine:
+                    print("Ha ganado {}".format(activeRunner.name))
+                    gameOver = True
                       
             pygame.display.flip()
-        
-        pygame.quit()
-        sys.exit()
+            
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
     
     
 if __name__ == "__main__":
